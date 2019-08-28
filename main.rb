6910cd744454cc7a91f4f25a18ef294b6a34cd68
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-28 15:52:14 +0000

    Set content types properly

--- main.rb
+++ main.rb
@@ -33,10 +33,12 @@ end
 
 get '/api' do
     cross_origin
+    content_type 'application/json'
     if params[:url]
         begin
             filename = juiceboxify(params[:url])
             if filename
+                content_type 'image/jpeg'
                 send_file File.join(Dir.pwd, "public/images/#{filename}"), disposition: :inline, type: "image/jpeg"
             else
                 JSON.generate({
